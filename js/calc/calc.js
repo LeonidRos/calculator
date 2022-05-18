9a26dc2f7b53db0d92d7e78aec9b5a2633f6a584
LeonidRos <102150359+LeonidRos@users.noreply.github.com>
2022-05-19 00:50:37 +0300

    80% done

--- js/calc/calc.js
+++ js/calc/calc.js
@@ -1,99 +1,135 @@
+
 const calc = function(){
 
     let calcInputElem = document.querySelector('#calc__input');
-    let calcBtnElems = document.querySelector('#calc__btns');
-    calcBtnElems = Array.from(calcBtnElems.children);
+    let calcBtnElems = Array.from(document.querySelector('#calc__btns').children);
 
-    let a = [],
-        i = 0,
-        mo = [];
+    let result = [],
+        index = 0,
+        mathOper = [];
+
+        // let counter = 0,
+        //     couter2 = 0;
+
+    
+
+    const reset = function(){
+
+        result = [];
+        index = 0;
+        mathOper = [];
+        // couter2 = 0,
+        // counter = 0;
+    };
+
+    const inputCalc = function(event){
+
+        if(!/[\+\*\/\-\%1-9.]/.test(event.key))calcInputElem.value = calcInputElem.value.replace(/[^\d\+\-\*\/]/g, '');
+
+        if(/[\+\-\*\/.]{2}/g.test(calcInputElem.value))calcInputElem.value = calcInputElem.value.replace(/([\+\-\*\/.]){1}/g, '');
+
+        // let arr = Array.from(calcInputElem.value);
+        // console.log(arr);
+
+        
+        // for(; couter2 < arr.length; couter2++, counter++){
+
+        //     if(
+        //         arr[couter2] === '+' ||
+        //         arr[couter2] === '-' ||
+        //         arr[couter2] === '*' ||
+        //         arr[couter2] === '/' ||
+        //         arr[couter2] === '%' 
+        //     )   counter--;
+
+        //         console.log('index ' + couter2)
+        //         console.log('counter ' + counter)
+
+                
+        //     if(counter != couter2-1){
+        //         if(
+        //             arr[couter2] === '+' ||
+        //             arr[couter2] === '-' ||
+        //             arr[couter2] === '*' ||
+        //             arr[couter2] === '/' ||
+        //             arr[couter2] === '%' 
+        //         ){
+        //             arr[couter2] = '';
+        //         }
+        //     }
+        // }
+
+        calcInputElem.value = arr.join('');
+
+        // if(/[\+\-\*\/.]/g.test(calcInputElem.value))calcInputElem.value = calcInputElem.value.replace(/[\+\-\*\/]$/, '');
+        
+    };
 
     const calculate = function(num = [], mathOp){
 
-        let result;
         switch(mathOp){
 
             case '+':
-                result = +num[0]+ +num[1];
-                calcInputElem.value = result;
+                calcInputElem.value = +num[0] + +num[1];
 
-                a = [];
-                i = 0;
-                mo = [];
-
-            return result;
+                reset();
+                break;
             
             case '-':
-                result = +num[0]- +num[1];
-                calcInputElem.value = result;
-                a = [];
-                i = 0;
-                mo = [];
-    
-                return result;
+                calcInputElem.value = +num[0] - +num[1];
+
+                reset();
+                break;
         
             case '*':
-                result = +num[0]* +num[1];
-                calcInputElem.value = result;
-                a = [];
-                i = 0;
-                mo = [];
-    
-                return result;
+                calcInputElem.value = +num[0] * +num[1];
+
+                reset();
+                break;
                 
             case '/':
-                result = +num[0]/ +num[1];
-                console.log(result)
-                calcInputElem.value = result;
-                a = [];
-                i = 0;
-                mo = [];
-    
-                return result;
+                calcInputElem.value = +num[0] / +num[1];
+
+                reset();
+                break;
         
             case '%':
-                result = +num[0]% +num[1];
-                calcInputElem.value = result;
-                a = [];
-                i = 0;
-                mo = [];
-    
-                return result;
+                calcInputElem.value = +num[0] % +num[1];
+
+                reset();
+                break;
         }
-        
     };
+
+    calcInputElem.addEventListener('keyup', inputCalc);
 
     calcBtnElems.forEach(elem => {
         elem.addEventListener('click', function(event){
             if(event.target.dataset.calc <= 9 && event.target.dataset.calc >= 0)calcInputElem.value += event.target.dataset.calc;
             else if(event.target.dataset.calc == '.' && !/[.]/g.test(calcInputElem.value))calcInputElem.value += event.target.dataset.calc;
+            else if(event.target.dataset.calc == 'rev')calcInputElem.value *= -1;
+            else if(event.target.dataset.calc == 'sqr')calcInputElem.value = Math.pow(calcInputElem.value, 2);
             else if(event.target.dataset.calc == 'c'){
                 calcInputElem.value = '';
-                a = [];
-                i = 0;
-                mo = [];
+
+                reset();
             };
 
             if(/[\+\*\/\-\%]/.test(event.target.dataset.calc)){
 
                 if(calcInputElem.value == '')return;
-                a[i] = calcInputElem.value;
-                mo[i] = event.target.dataset.calc;
+
+                result[index] = calcInputElem.value;
+                mathOper[index] = event.target.dataset.calc;
                 calcInputElem.value = '';
-                i++;
-                if(a.length >= 2)calculate(a, mo[i-2]);
+                index++;
+                if(result.length >= 2)calculate(result, mathOper[index-2]);
             }
+
             if(event.target.dataset.calc == '='){
-                a[i] = calcInputElem.value;
-                calculate(a, mo[i-1]);
+                result[index] = calcInputElem.value;
+                calculate(result, mathOper[index-1]);
             }
         });
     });
-
-    console.log(calcInputElem);
 };
-
-// \+\-\*\/%\.
-
-// else if(event.target.dataset.calc == 'rev')calcInputElem.value *= -1;
-// else if(!/\D$/g.test(calcInputElem.value) && !/\D/g.test(calcInputElem.value))calcInputElem.value += event.target.dataset.calc;
